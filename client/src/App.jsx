d55613f6dd62f0edc14a3904932f520ae8b31f06
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-12-02 21:30:55 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-=======
 import './App.css';
 import Navbar from './components/navbar';
->>>>>>> cadc333 (app colors)
 
 function App() {
   return (
