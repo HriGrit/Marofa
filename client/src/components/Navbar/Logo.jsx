6d31886f8dc86e6aae8b2b28147ccd772fdca320
Ayush Soam <ayushsoam15@gmail.com>
2023-12-02 23:26:08 +0530

    body

--- client/src/components/Navbar/Logo.jsx
+++ client/src/components/Navbar/Logo.jsx
@@ -1,10 +1,10 @@
 import { Box, styled } from '@mui/material';
 
 const StyledLogo = styled('img')({});
-import logo from "../../assets/marofa-logo-dark.svg";
+import logo from "../../assets/logo/marofa-logo-dark.svg";
 
 const Logo = (props) => {
-    return <StyledLogo src={logo} alt="Logo" width={"30px"}{...props} />;
+    return <StyledLogo src={logo} alt="Logo" width={"69px"}{...props} />;
 };
 
 
