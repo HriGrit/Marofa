b994a3030f5dcbce367fe43d7514c1046088e61f
Ayush Soam <ayushsoam15@gmail.com>
2023-12-02 23:33:20 +0530

    yes

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
 import Container from "@mui/material/Container";
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 const Body = () => {
     return (
-        <Container>
-            <Typography>
-                <Box sx={{ fontFamily: "NeoSans", }}>
-                    Welcome to Marofa!
-                    I am in here
-                </Box>
-            </Typography>
+        <Container maxWidth="xl" sx={{ mt: 4 }}>
+            <Box>
+                <Grid container spacing={2} sx={{ backgroundColor: "black" }}>
+                    <Grid item sm={12} md={7} sx={{ backgroundColor: "red" }}>
+                        <Item>
+                            <h1>Body</h1>
+                        </Item>
+                    </Grid>
+                    <Grid item md={5} sx={{ backgroundColor: "blue", display: { sm: 'none', md: 'block' } }}>
+                        <Item>
+                            <h1>Body</h1>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </Container >
     )
 }
