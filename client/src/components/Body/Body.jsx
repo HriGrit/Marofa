c8de91989e9c00f3f9243b710cc85ecd7d3f8480
Syed <junaid.nihal@gmail.com>
2023-12-02 22:19:20 +0300

    img

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Item from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
-import lady from "../../assets/landingpagefemale.svg"
+import lady from "../../assets/landingpagefemale.svg";
 
 const Body = () => {
     return (
-        <Container maxWidth="xl" sx={{ mt: 4, padding: 0, backgroundColor: "#F9F9F9" }}> {/* Remove container padding */}
+        <Container maxWidth="xxl" sx={{ mt: 4, padding: 0, backgroundColor: "#F9F9F9" }}> {/* Remove container padding */}
             <Box sx={{ flexGrow: 1, width: '100%' }}>
                 <Grid container spacing={0}>
-                    <Grid item xs={12} md={6} sx={{ px: 0, mx: 0 }}> {/* Remove grid item padding */}
+                    <Grid item xs={'12'} md={6} sx={{ px: 0, mx: 0 }}> {/* Remove grid item padding */}
                         <h1>Body</h1>
                     </Grid>
                     <Grid item md={6} sx={{ display: { xs: 'none', md: 'block' }, paddingLeft: 0, paddingRight: 0 }}> {/* Remove grid item padding */}
                         {/* Your Item component here */}
-                        <img height={"500px"} src={lady} alt="lady" style={{ width: '100%', height: 'auto' }} />
+                        <img
+
+                            height={"500px"}
+                            src={lady}
+                            alt="lady"
+                            style={
+                                {
+                                    width: '100%',
+                                    height: 'auto',
+                                }
+                            } />
                     </Grid>
                 </Grid>
             </Box>
         </Container >
-    )
-}
+    );
+};
 
-export default Body
+export default Body;
