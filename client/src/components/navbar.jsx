c3d9a0c485476b9f0c842d54167e3a41236f7f9f
Syed Hussaini <syedhussaini@Syeds-MacBook-Pro.local>
2023-12-02 19:39:10 +0300

    font test

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
+                            fontFamily: 'NeoSans',
                             fontWeight: 700,
                             letterSpacing: '.3rem',
                             color: 'inherit',
