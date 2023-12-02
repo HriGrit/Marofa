efd8634a2a20f4f6e87f6f1ee8fb58394f9ad176
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-12-02 22:16:59 +0530

    body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar/navbar';
 
 function App() {
   return (
