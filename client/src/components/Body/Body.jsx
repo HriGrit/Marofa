3f88375da7bd3204ef4eaea9311a1035f7b06ac6
Ayush Soam <ayushsoam15@gmail.com>
2023-12-03 00:12:38 +0530

    bodybody

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -5,20 +5,19 @@ import Grid from "@mui/material/Grid";
 import Item from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
+import lady from "../../assets/landingpagefemale.svg"
+
 const Body = () => {
     return (
-        <Container maxWidth="xl" sx={{ mt: 4 }}>
-            <Box>
-                <Grid container spacing={2} sx={{ backgroundColor: "black" }}>
-                    <Grid item sm={12} md={7} sx={{ backgroundColor: "red" }}>
-                        <Item>
-                            <h1>Body</h1>
-                        </Item>
+        <Container maxWidth="xl" sx={{ mt: 4, padding: 0, backgroundColor: "#F9F9F9" }}> {/* Remove container padding */}
+            <Box sx={{ flexGrow: 1, width: '100%' }}>
+                <Grid container spacing={0}>
+                    <Grid item xs={12} md={6} sx={{ px: 0, mx: 0 }}> {/* Remove grid item padding */}
+                        <h1>Body</h1>
                     </Grid>
-                    <Grid item md={5} sx={{ backgroundColor: "blue", display: { sm: 'none', md: 'block' } }}>
-                        <Item>
-                            <h1>Body</h1>
-                        </Item>
+                    <Grid item md={6} sx={{ display: { xs: 'none', md: 'block' }, paddingLeft: 0, paddingRight: 0 }}> {/* Remove grid item padding */}
+                        {/* Your Item component here */}
+                        <img height={"500px"} src={lady} alt="lady" style={{ width: '100%', height: 'auto' }} />
                     </Grid>
                 </Grid>
             </Box>
