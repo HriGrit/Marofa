3375d48fd3688e540eca92e79f4e18f359d521a8
Syed <junaid.nihal@gmail.com>
2023-12-02 20:49:48 +0300

    logo size

--- client/src/components/Navbar/Logo.jsx
+++ client/src/components/Navbar/Logo.jsx
@@ -4,8 +4,8 @@ const StyledLogo = styled('img')({});
 import logo from "../../assets/marofa-logo-dark.svg";
 
 const Logo = (props) => {
-    return <StyledLogo src={logo} alt="Logo" width={"30px"}{...props} />;
+    return <StyledLogo src={logo} alt="Logo" width={"70px"}{...props} />;
 };
 
 
-export default Logo
+export default Logo;
