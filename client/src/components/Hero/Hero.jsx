48365e7dc740926632164c510f7b66bec8c061f8
Syed <junaid.nihal@gmail.com>
2023-12-05 20:34:41 +0300

    deleted HERO T_T

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,117 +1,22 @@
 import ladyImg from "../../assets/landingpagefemale.svg";
-import { Box, Button, styled, Typography } from "@mui/material";
-import { Container } from "@mui/system";
 import React from "react";
-import CustomButton from "./CustomButton";
-import Lady from "../Body/Lady";
-import { left } from "@popperjs/core";
 
 const Hero = () => {
-  const CustomBox = styled(Box)(({ theme }) => ({
-    fontFamily: 'NeoSans',
-    display: "flex",
-    justifyContent: "center",
-    gap: theme.spacing(5),
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(4),
-    marginRight: theme.spacing(4),
-    marginBottom: theme.spacing(3),
-    [theme.breakpoints.down("md")]: {
-      flexDirection: "column",
-      alignItems: "center",
-      textAlign: "center",
-    },
-  }));
-
-  const Title = styled(Typography)(({ theme }) => ({
-    fontFamily: "Neosans",
-    fontSize: "64px",
-    color: "#000336",
-    fontWeight: "bold",
-    margin: theme.spacing(4, 0, 4, 0),
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "40px",
-    },
-  }));
-
   return (
-    <Box sx={{ backgroundColor: "#E6F0FF", }}>
-      <Container maxWidth="100%" disableGutters >
-        <CustomBox>
-          <Box sx={{ flex: "1" }}
-          >
-            <Typography
-
-              variant="body2"
-              sx={{
-                fontFamily: "Neosans",
-                fontSize: "18px",
-
-                color: "#687690",
-                fontWeight: "500",
-                mt: 10,
-              }}
-            >
-              Looking for a house help?
-            </Typography>
-            <Title sx={{
-              fontFamily: "Neosans",
-              fontSize: { xs: "36px", lg: "48px", xl: "96px" },
-              color: "#0F1B4C",
-              fontWeight: "500",
-            }}>
-              Find available helpers and maids in Saudi Arabia
-            </Title>
-            <Typography
-              variant="body2"
-              sx={{ fontFamily: "Neosans", fontSize: { xs: "16px", lg: "18px" }, color: "#5A6473", my: "4" }}
-            >
-              Explore Profiles and contact helpers directly
-            </Typography>
-            <Box sx={{
-              display: {
-                sm: 'block', md: 'flex', lg: 'flex',
-                justifyContent: 'flex-start',
-                marginTop: '50px',
-                marginBottom: '20px'
-              }
-            }}>
-              <CustomButton
-                className="button"
-                backgroundColor="#0F1B4C"
-                color="#fff"
-                buttonText="For Employers"
-                heroBtn={true}
-              />
-              <CustomButton
-                className="button"
-                backgroundColor="#0F1B4C"
-                color="#fff"
-                buttonText="For Helpers"
-                heroBtn={true}
-                sx={{ marginLeft: '120px' }}
-              />
-            </Box>
-          </Box>
-
-
-          <Box sx={{
-            flex: "1.25",
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: 'auto',
-            height: 'auto',
-
-
-
-          }}>
-            <Lady sx={{ height: 'auto', maxWidthWidth: 'auto' }} />
-          </Box>
-
-        </CustomBox>
-      </Container>
-    </Box >
+    <section className="flex flex-col md:flex-row items-center bg-gray-100 p-4 md:p-8">
+      <div className="flex-1 space-y-4">
+        <h1 className="text-2xl md:text-3xl font-bold text-gray-800">Looking for a House help?</h1>
+        <h2 className="text-xl md:text-2xl text-gray-700">Find available helpers and maids in Saudi Arabia</h2>
+        <p className="text-gray-600">Explore profiles and contact helpers directly</p>
+        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">For Employers</button>
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300">For Helpers</button>
+        </div>
+      </div>
+      <div className="flex-1 mt-8 md:mt-0">
+        <img src={ladyImg} alt="House helper" className="w-full h-auto" />
+      </div>
+    </section>
   );
 };
 
