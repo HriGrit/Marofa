82074c7030161d847c697bdac6a2ee4a350bcd07
Ayush Soam <ayushsoam15@gmail.com>
2023-12-06 22:28:39 +0530

    server mvc setup

---
+++ server/controllers/user/userController.js
@@ -0,0 +1,8 @@
+// sample create function
+const User = require("../../models/user/user");
+
+const createUserController = async (req, res) => {
+	console.log("createUserController");
+};
+
+module.exports = { createUserController };
