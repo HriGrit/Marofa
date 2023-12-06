7d439325e8b2f789133aa0a900f44510c62f4483
Syed <junaid.nihal@gmail.com>
2023-12-06 20:48:02 +0300

    footer

--- client/src/components/Body/Footer/Footer.jsx
+++ client/src/components/Body/Footer/Footer.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import logo from '../../../assets/marofa-logo-footer.png';
 const Footer = () => {
     return (
-        <div className='flex justify-between'>
-            <div className=''>
-                marofa
-            </div>
-            <div className=' bg-red-100'>
-                join
-            </div>
-            <div className=' bg-red-200'>
-                sign in
-            </div>
-        </div>
-    )
-}
+        <footer class="bg-[#F3F3F3]">
+            <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div class="md:flex md:justify-between">
+                    <div class="mb-6 md:mb-0">
+                        <a href="https://flowbite.com/" class="flex items-center">
+                            <img src={logo} class="h-[100px]  me-3 md:h-[140px]" alt="Footer logo" />
+                            <span class="self-center text-4xl font-[#14415A] font-semibold whitespace-nowrap md:text-6xl">MAROFA</span>
+                        </a>
 
-export default Footer
+                    </div>
+
+                </div>
+
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
