41221d798c86c8819cfd7a33050c3968810f7495
Ayush Soam <ayushsoam15@gmail.com>
2023-12-08 02:11:44 +0530

    deleted somthn

--- client/src/components/Body/FAQ/FAQ.jsx
+++ client/src/components/Body/FAQ/FAQ.jsx
@@ -4,17 +4,6 @@ import dropdown from "../../../assets/dropdown.svg";
 const FAQ = () => {
     return (
         <div>
-            <div className='dimmed-faq h-[450px] bg-contain'>
-                <div className='h-full flex flex-col justify-center pt-32 gap-4'>
-                    <div className='text-center w-full text-white'>
-                        <p>Benefit from full access to our database of helpers and </p>
-                        <p>Get in touch with them directly</p>
-                    </div>
-                    <div className='text-center'>
-                        <button className='p-3 px-12 bg-[#123750] text-white rounded-[24px] text-lg hover:bg-[#2E72D9] font-semibold'>Get Started</button>
-                    </div>
-                </div>
-            </div>
             <div className='dimmed-bg space-y-12 pb-20'>
                 <div className='text-center pt-20 space-y-3 '>
                     <h2 className=' text-md'>Need Help?</h2>
