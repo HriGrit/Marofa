1c1c70f70ec73caf983eadc5ff12431ca56aa44e
Syed <junaid.nihal@gmail.com>
2023-12-10 21:24:59 +0300

    getStarted

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,47 +1,24 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
 import logo from '../../assets/marofa-logo-dark.svg';
-
-
-const pages = ['Home', 'Pricing', 'How it Works'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-const auth = ["Register", "Login"];
+import { useState } from "react";
+import Modal from "../Modals/Modal";
+import GetStartedContent from "../Modals/GetStartedContent";
 
 import Logo from './Logo';
 
+
 function Navbar() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-
-
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
+    // Event handler for opening the modal
+    const openModal = () => {
+        setIsModalOpen(true);
     };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
+    // Event handler for closing the modal
+    const closeModal = () => {
+        setIsModalOpen(false);
     };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
     return (
 
 
@@ -52,13 +29,24 @@ function Navbar() {
                     <span class="self-center font-semibold text-white">MAROFA</span>
                 </a>
                 <div class="flex mdnav:order-2 space-x-3 mdnav:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-[#14415a] font-thin animate-pulse tracking-widest text-l px-4 py-2 text-center whitespace-nowrap hover:ring-2 hover:ring-white rounded-full focus:outline-none focus:ring-2 focus:ring-white hover:animate-none">Get started</button>
+                    <button
+                        onClick={openModal}
+                        type="button"
+                        class="text-white bg-[#14415a] font-thin animate-pulse tracking-widest text-l px-4 py-2 text-center whitespace-nowrap hover:ring-2 hover:ring-white rounded-full focus:outline-none focus:ring-2 focus:ring-white hover:animate-none">
+                        Get started
+                    </button>
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg mdnav:hidden hover:bg-[#2E72D9] focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-cta" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
+                    <div>
+                        <Modal open={isModalOpen} onClose={closeModal}>
+                            <GetStartedContent />
+                        </Modal>
+                    </div>
+
                 </div>
                 <div class="items-center justify-between hidden w-full mdnav:flex mdnav:w-auto mdnav:order-1 pl-10 pr-10 " id="navbar-cta">
                     <ul class="flex flex-col font-medium p-4 mdnav:p-0 mt-4 border border-gray-100 rounded-lg bg-[#14415a] mdnav:space-x-[50px] rtl:space-x-reverse mdnav:flex-row mdnav:mt-0 mdnav:border-0 mdnav:bg-[#14415a]">
@@ -81,6 +69,7 @@ function Navbar() {
                 </div>
             </div>
         </nav>
+
 
     );
 }
