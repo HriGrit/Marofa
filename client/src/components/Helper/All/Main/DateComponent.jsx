a7e9a9e974d566417bbb48cf9f0dc606d8780d3f
Ayush Soam <ayushsoam15@gmail.com>
2023-12-12 20:05:35 +0530

    date and location components

---
+++ client/src/components/Helper/All/Main/DateComponent.jsx
@@ -0,0 +1,105 @@
+
+import React, { useEffect, useState } from 'react'
+
+import DatePicker from 'react-datepicker'
+import { getMonth, getYear } from 'date-fns'
+
+import './DateComponent.css'
+import 'react-datepicker/dist/react-datepicker.css'
+
+const DateComponent = ({ onChangeDate, date, isreset, placeholderText }) => {
+    const getYearRange = (start, end) => {
+        const years = [];
+        for (let year = start; year <= end; year++) {
+            years.push(year);
+        }
+        return years;
+    };
+
+    const currentYear = new Date().getFullYear();
+    const years = getYearRange(2020, currentYear + 5);
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    useEffect(() => {
+        if (isreset) {
+            onChangeDate(null);
+        }
+    }, [isreset, onChangeDate]);
+
+    return (
+        <div className="relative inline-block w-full text-gray-700">
+            <label className="block text-sm font-medium text-gray-700" htmlFor="candidate-location">
+                {placeholderText}
+            </label>
+            <DatePicker
+                wrapperClassName="react-datepicker"
+                className='p-1'
+                isClearable
+                placeholderText='dd-mm-yyyy'
+                renderCustomHeader={({
+                    date,
+                    changeYear,
+                    changeMonth,
+                    decreaseMonth,
+                    increaseMonth,
+                    prevMonthButtonDisabled,
+                    nextMonthButtonDisabled,
+                }) => (
+                    <div
+                        className='flex flex-row justify-evenly items-center space-x-4 text-lg'
+                    >
+                        <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className='text-xl'>
+                            {"<"}
+                        </button>
+                        <select
+                            value={getYear(date)}
+                            onChange={({ target: { value } }) => changeYear(value)}
+                            className=' bg-inherit outline-none appearance-none px-0.25'
+                        >
+                            {years.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+
+                        <select
+                            value={months[getMonth(date)]}
+                            onChange={({ target: { value } }) =>
+                                changeMonth(months.indexOf(value))
+                            }
+                            className=' outline-none appearance-none bg-inherit text-center'
+                        >
+                            {months.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+
+                        <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className='text-xl'>
+                            {">"}
+                        </button>
+                    </div>
+                )}
+                selected={date}
+                onChange={(date) => onChangeDate(date)}
+            />
+        </div>
+    )
+}
+
+export default DateComponent
--- client/src/components/Helper/All/Main/HelperFilter.jsx
+++ client/src/components/Helper/All/Main/HelperFilter.jsx
@@ -1,26 +1,53 @@
 import React, { useState } from 'react';
 import reload from '../../../../assets/reload.svg';
+import DateComponent from './DateComponent';
+import HrLine from './HrLine';
+import MultiSelectComponent from './MultiSelectComponent';
 
 const HelperFilter = () => {
     const [startDate, setStartDate] = useState(null);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [reset, setReset] = useState(false);
+
+    const handleOnReset = () => {
+        setReset(!reset);
+    }
+
+    const LocationOptions = [
+        { value: "select-all", label: "Select All", color: "#14415A" },
+        { value: "Saudi Arabia", label: "Saudi Arabia", color: "#14415A" },
+        { value: "Indonesia", label: "Indonesia", color: "#14415A" },
+        { value: "Malaysia", label: "Malaysia", color: "#14415A" },
+        { value: "Hong Kong", label: "Hong Kong", color: "#14415A" },
+        { value: "India", label: "India", color: "#14415A" },
+        { value: "Kenya", label: "Kenya", color: "#14415A" },
+        { value: "South Africa", label: "South Africa", color: "#14415A" },
+        { value: "Sri Lanka", label: "Sri Lanka", color: "#14415A" },
+        { value: "Thailand", label: "Thailand", color: "#14415A" },
+        { value: "United Arab Emirates", label: "United Arab Emirates", color: "#14415A" },
+        { value: "Qatar", label: "Qatar", color: "#14415A" },
+        { value: "Jordan", label: "Jordan", color: "#14415A" }
+    ];
 
     return (
         <div className='bg-red-200 w-1/5 p-4'>
-            <div>
+            <div className='space-y-2'>
                 <p>I am looking for</p>
                 <div className='flex flex-row justify-between'>
                     <p>Filter</p>
-                    <button className='flex flex-row items-center'>
+                    <button className='flex flex-row items-center' onClick={handleOnReset}>
                         <img src={reload} alt='reload' className='w-4 h-4 mr-1' />
-                        <p>Reset</p>
+                        <p className=''>Reset</p>
                     </button>
                 </div>
                 <div>
-                    <input
-                        type='date'
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                    />
+                    <DateComponent onChangeDate={setStartDate} date={startDate} isreset={reset} placeholderText={"Start Date"} />
+                </div>
+                <div>
+                    <MultiSelectComponent onChangeLocation={setSelectedOptions} location={selectedOptions} isreset={reset} placeholdertext={"Select Location"} options={LocationOptions} />
+                </div>
+                <div>
+                    <HrLine />
                 </div>
             </div>
         </div>
