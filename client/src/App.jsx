72fb796d9706d8d7fc1cb71478ad281d5c1eb31a
Syed <junaid.nihal@gmail.com>
2023-12-13 02:41:06 +0300

    added lazy loading, 404, and Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import './App.css';
-
-import Home from './pages/Home/Home';
-
+import React, { Suspense, lazy } from 'react';
+const Home = lazy(() => import('./pages/Home/Home'));
+const LoginPage = lazy(() => import('./components/Body/Register/LoginPage'));
 import AllEmployers from './pages/Employer/AllEmployers';
 import SingleEmployer from './pages/Employer/SingleEmployer';
 import AllHelpers from './pages/Helper/AllHelpers';
 import SingleHelper from './pages/Helper/SingleHelper';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestorev } from 'firebase/firestore';
-const firebaseConfig = {
-  //...
-};
-
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/employers" element={<AllEmployers />} />
-        <Route path="/employers/:employerId" element={<SingleEmployer />} />
-        <Route path="/helpers" element={<AllHelpers />} />
-        <Route path="/helpers/:helperId" element={<SingleHelper />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path="/employers" element={<AllEmployers />} />
+          <Route path="/employers/:employerId" element={<SingleEmployer />} />
+          <Route path="/helpers" element={<AllHelpers />} />
+          <Route path="/helpers/:helperId" element={<SingleHelper />} />
+          <Route path='*' element={<div>Not Found</div>} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
