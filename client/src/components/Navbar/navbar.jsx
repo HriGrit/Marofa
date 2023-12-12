a90d2e7f756ed7ee27f608c7c292791cbe9cdf81
Syed <junaid.nihal@gmail.com>
2023-12-13 02:41:36 +0300

    changed button to Link for Get Started

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import logo from '../../assets/marofa-logo-dark.svg';
+import { Link } from 'react-router-dom';
 import { useState } from "react";
 import Modal from "../Modals/Modal";
 import GetStartedContent from "../Modals/GetStartedContent";
@@ -8,17 +9,28 @@ import Logo from './Logo';
 
 
 function Navbar() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    //     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // Event handler for opening the modal
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+    //     // Event handler for opening the modal
+    //     const openModal = () => {
+    //         setIsModalOpen(true);
+    //     };
 
-    // Event handler for closing the modal
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+    //     // Event handler for closing the modal
+    //     const closeModal = () => {
+    //         setIsModalOpen(false);
+    //     };
+    //     <div>
+    //     <Modal open={isModalOpen} onClose={closeModal}>
+    //         <GetStartedContent />
+    //     </Modal>
+    // </div>
+    //  <button
+    //  onClick={openModal}
+    //  type="button"
+    //  class="text-white bg-[#14415a] font-thin animate-pulse tracking-widest text-l px-4 py-2 text-center whitespace-nowrap hover:ring-2 hover:ring-white rounded-full focus:outline-none focus:ring-2 focus:ring-white hover:animate-none">
+    //  Get started
+    // </button>
     return (
 
 
@@ -29,23 +41,17 @@ function Navbar() {
                     <span class="self-center font-semibold text-white">MAROFA</span>
                 </a>
                 <div class="flex mdnav:order-2 space-x-3 mdnav:space-x-0 rtl:space-x-reverse">
-                    <button
-                        onClick={openModal}
+                    <Link to="/login"
                         type="button"
                         class="text-white bg-[#14415a] font-thin animate-pulse tracking-widest text-l px-4 py-2 text-center whitespace-nowrap hover:ring-2 hover:ring-white rounded-full focus:outline-none focus:ring-2 focus:ring-white hover:animate-none">
                         Get started
-                    </button>
+                    </Link>
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg mdnav:hidden hover:bg-[#2E72D9] focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-cta" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div>
-                        <Modal open={isModalOpen} onClose={closeModal}>
-                            <GetStartedContent />
-                        </Modal>
-                    </div>
 
                 </div>
                 <div class="items-center justify-between hidden w-full mdnav:flex mdnav:w-auto mdnav:order-1 pl-10 pr-10 " id="navbar-cta">
