6278e5f443b345db4cda5ac8a85fc9959214890b
Ayush Soam <ayushsoam15@gmail.com>
2024-02-05 12:37:05 +0530

    push

--- client/src/components/Register/UploadImage.jsx
+++ client/src/components/Register/UploadImage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import logo from '../../assets/marofa-whitebg.svg';
-const uploadpic = React.lazy(() => import('../../assets/uploadpic.png'));
+const uploadpic = React.lazy(() => import('../../assets/Uploadpic.png'));
 import Navbar from '../Navbar/navbar';
 import { uploadBytes, getDownloadURL, ref } from 'firebase/storage'; // Import Firebase Storage methods
 import { storage } from '../../utils/firebase'; // Import your Firebase storage instance
