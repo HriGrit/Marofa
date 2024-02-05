83eee48487600abba9f1e952ed96e33d2357c5cd
Ayush Soam <ayushsoam15@gmail.com>
2024-02-05 12:23:30 +0530

    Fix Minor Bug

--- client/src/components/Register/MultiStepForm.jsx
+++ client/src/components/Register/MultiStepForm.jsx
@@ -64,7 +64,6 @@ const MultiStepForm = () => {
             educationLevel: '',
             religion: '',
             currentContractStatus: '',
-            experience: '',
         },
         aboutEmployer: {
             employerType: '',
